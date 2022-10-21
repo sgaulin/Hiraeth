f52ab9c19f1686b137b7b51b4e412ae0789428ac
sgaulin <101838699+sgaulin@users.noreply.github.com>
2022-10-21 18:14:28 -0400

    fix arbes, synx clouds, fix stairs, add fx + sfx bridge, sync bridge

--- Hiraeth/Assets/Scripts/MovingClouds.cs
+++ Hiraeth/Assets/Scripts/MovingClouds.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
-using System.Collections;
-
 
 public class MovingClouds : MonoBehaviour
 {
-    public Rigidbody rb;
+    //public Rigidbody rb;
 
     public float speed = 1.0f;
-    public Vector3 direction = new Vector3(1,0,0);
+    public Vector3 direction = new Vector3(1,0,0);     
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();            
     }
-
+ 
     private void FixedUpdate()
     {
-        rb.AddForce(direction * speed);
+        //rb.AddForce(direction * speed);
+        transform.position += direction * speed;
     }
 }

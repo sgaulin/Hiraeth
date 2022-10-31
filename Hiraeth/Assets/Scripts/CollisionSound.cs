ebeb264ac62d04a71392f413791b865ceaec717e
sgaulin <101838699+sgaulin@users.noreply.github.com>
2022-10-31 01:50:09 -0400

    activation particles & sound on trees+ pedestal + crystal + bridge + stairs + rocks + water + doors, collisions sounds (same items), doors opening event

--- Hiraeth/Assets/Scripts/CollisionSound.cs
+++ Hiraeth/Assets/Scripts/CollisionSound.cs
@@ -4,37 +4,55 @@ using UnityEngine;
 
 public class CollisionSound : MonoBehaviour
 {
-    public AudioSource hitSound;
+    [Header("Collision")]
+    private AudioSource hitSound;
     public float velocityThreshold = 1;
+    public float repeatPercent = 1;
+    [Space]
+    public float minPitch = 0.8f;
+    public float maxPitch = 1.2f;
 
+    [Header("Sploush")]
     public GameObject sploushFX;
     public float destroyDelay = 5.0f;
-	
-	//private float randomValue = 0;
+
+    private float nextSoundTime = 0;
+
 
     private void Start()
     {
-        hitSound = GetComponent<AudioSource>();
+        hitSound = GetComponent<AudioSource>();        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Debug.Log("Collision" + collision.gameObject.name);
         if(collision.relativeVelocity.magnitude >= velocityThreshold)
         {
-            if (!hitSound.isPlaying)
+            if (Time.time>=nextSoundTime)
             {
-				//random;
-                hitSound.Play();
+                if(this.tag != collision.collider.tag && collision.collider.tag != "Water")
+                {
+                    hitSound.pitch = Random.Range(minPitch, maxPitch);
+                    hitSound.Play();
+                    nextSoundTime = Time.time + ( hitSound.clip.length * repeatPercent );
+                }				
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (this.tag == "Water")
+        if (Time.time >= nextSoundTime)
         {
-            GameObject sploush = (GameObject)Instantiate(sploushFX, other.transform.position, Quaternion.identity);
-            Destroy(sploush, destroyDelay);
+            if (this.tag == "Water" && (other.tag == "Rocks" || other.tag == "Tree" || other.tag == "Crystal" || other.tag == "Giant"))
+            {
+                Debug.Log("splash" + other.gameObject.name);                
+                nextSoundTime = Time.time + (sploushFX.GetComponentInChildren<AudioSource>().clip.length * repeatPercent);
+                GameObject sploush = (GameObject)Instantiate(sploushFX, other.transform.position, Quaternion.identity);
+                sploush.GetComponentInChildren<AudioSource>().pitch = Random.Range(minPitch, maxPitch);
+                Destroy(sploush, destroyDelay);                
+            }
         }
     }
 }
